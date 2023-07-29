6d9090320b23637afb91f218f11405b5884428f7
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-07-29 16:46:46 +0300

    vimeo, throttle та 2-ге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,34 @@
 import VimeoPlayer from "@vimeo/player"
+import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
     const player = new VimeoPlayer(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+function onPlay ( timeupdate) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(timeupdate.seconds))
+    
+  }
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.on('timeupdate', throttle(onPlay, 1000))
+const data = localStorage.getItem("videoplayer-current-time")
+const parsedData = JSON.parse(data)
+player.setCurrentTime(parsedData)
+
+
+
+
+
+
+
+// .then(function (seconds){}).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// })
+
