e2357196939eb9eca978503070fbc8de45ff23fb
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-07-29 19:00:27 +0300

    виконане 3-тє завдання

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from "lodash.throttle"
 
+
+const form = document.querySelector('.feedback-form')
+const input = document.querySelector("input")
+const textarea = document.querySelector("textarea")
+const STORAGE_KEY = "feedback-form-state"
+
+
+form.addEventListener('input', throttle(inputData, 500));
+form.addEventListener('submit', submitData)
+auto()
+function inputData() {
+    const data = {}
+    data.email = input.value
+    data.message = textarea.value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
+    console.log(data);
+   
+    
+}
+
+function auto() {
+
+    const item = localStorage.getItem(STORAGE_KEY)
+    const parsedItem = JSON.parse(item)
+    if (parsedItem) {
+    textarea.value = parsedItem.message;
+    input.value = parsedItem.email
+    }
+}
+
+function submitData(event) {
+    if (event.target.nodeName ==! 'BUTTON') 
+        return
+   
+    localStorage.clear()
+}
